6323912e7b8e57e837cdc4a7de706ec6b1cd96a7
GiorgiaMannino <giorgimannino@hotmail.com>
2025-05-17 13:15:31 +0200

    configurazione file

---
+++ src/main/java/it/epicode/Capstone_ReGive_back_end/CapstoneReGiveBackEndApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.Capstone_ReGive_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneReGiveBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneReGiveBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/Capstone_ReGive_back_end/CapstoneReGiveBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Capstone_ReGive_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneReGiveBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
